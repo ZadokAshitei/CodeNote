f940a00d9d0524271ace6bbcfd308a72b0ebbb30
Zadok Armah <zadok@Zadoks-MacBook-Pro.local>
2019-04-04 13:48:28 +0000

    First Commit

---
+++ app/Http/Controllers/NotebooksController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\NoteBook;
+
+class NotebooksController extends Controller
+{
+    public function showAddNotebookForm()
+    {
+        return view('notebooks/add-notebook');
+    }
+
+    public function addNotebook(Request $request)
+    {
+        $title = $request->title;
+        NoteBook::create(['title'=>$title]);
+        return redirect()->route('notebooks');
+    }
+
+    public function showNotebooks()
+    {
+        $notebooks = NoteBook::all();
+        return view('notebooks/notebooks', compact('notebooks'));  //compact() send data to the view so that we can access it there
+    }
+
+    public function destroy($id)
+    {
+        Notebook::destroy($id);
+        return redirect()->route('notebooks');
+    }
+
+    public function showEditForm($id)
+    {
+        $notebook = Notebook::find($id);
+        return view('notebooks/updateNotebook', compact('id', 'notebook'));
+    }
+
+    public function update(Request $request, $id)
+    {
+      $title = $request->title;
+      $notebook = NoteBook::find($id);
+      $notebook->update(['title'=>$title]);
+      return redirect()->route('notebooks');
+    }
+}
---
+++ app/Notebook.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notebook extends Model
+{
+    protected $fillable = ['title'];
+
+    public function notes()
+    {
+      return $this->hasMany('App\Note');
+    }
+}
